b5fd93242534b4ac68b59d512050da5b662e5280
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-22 15:38:14 +0530

    setup routes

---
+++ src/app/packages/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function packages() {
+  return (
+    <div>packages</div>
+  )
+}
+
+export default packages
---
+++ src/app/places/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function places() {
+  return (
+    <div>places</div>
+  )
+}
+
+export default places
