96076a6fc83fc34a226be6bbb12a8c9c8ec58a61
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-22 16:04:35 +0530

    landing page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-      This is Home Page Of the Website
+    <div className="text-center">
+      <h1 className="mb-10 mt-10">
+        This is Home Page Of the Website
       </h1>
-      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-primary m-80">Button</button>
     </div>
   );
 }
