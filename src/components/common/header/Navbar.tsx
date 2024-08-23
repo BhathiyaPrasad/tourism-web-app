0bf841e74bbc3c145b3cb6c4a9a82d4ffb29d72b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-23 14:22:24 +0530

    testing translation

--- src/components/common/header/Navbar.tsx
+++ src/components/common/header/Navbar.tsx
@@ -1,57 +1,58 @@
 import React from "react"
 import Link from "next/link";
-import GoogleTranslate from './../../GoogleTranslate';
+
 
 const Navbar = () => {
 
-    return(
-<>
-    <div className="navbar bg-base-20">
-        <div className="navbar-start">
-            <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
+    return (
+        <>
+            <div className="navbar bg-base-20">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><Link href='/'>Home</Link></li>
+                            <li><Link href='/packages'>Packages</Link></li>
+                            <li><Link href='/places'>Places</Link></li>
+                            <li><Link href='/about'>About Us</Link></li>
+                            <li><Link href='/contact'>Contact</Link></li>
+                            <li><Link href='/about'>About Us</Link></li>
+                        </ul>
+                    </div>
+                    <Link href='' className="btn btn-ghost text-xl">Jagath Travels</Link>
                 </div>
-                <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/packages'>Packages</Link></li>
-                    <li><Link href='/places'>Places</Link></li>
-                    <li><Link href='/about'>About Us</Link></li>
-                    <li><Link href='/contact'>Contact</Link></li>
-                    <li><Link href='/about'>About Us</Link></li>
-                </ul>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li className="text-base mr-5"><Link href='/'>Home</Link></li>
+                        <li className="text-base mr-5"><Link href='/packages'>Packages</Link></li>
+                        <li className="text-base mr-5"><Link href='/places'>Places</Link></li>
+                        <li className="text-base mr-5"><Link href='/about'>About Us</Link></li>
+                        <li className="text-base mr-5"><Link href='/contact'>Contact</Link></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+             
+
+
+                </div>
             </div>
-            <Link href='' className="btn btn-ghost text-xl">Jagath Travels</Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-                <li className="text-base mr-5"><Link href='/'>Home</Link></li>
-                <li className="text-base mr-5"><Link href='/packages'>Packages</Link></li>
-                <li className="text-base mr-5"><Link href='/places'>Places</Link></li>
-                <li className="text-base mr-5"><Link href='/about'>About Us</Link></li>
-                <li className="text-base mr-5"><Link href='/contact'>Contact</Link></li>
-            </ul>
-        </div>
-        <div className="navbar-end">
-            <Link href='' className="btn"><GoogleTranslate/></Link>
-            
-        </div>
-    </div>
-</>
-);
+        </>
+    );
 };
 
 export default Navbar
