5a3b1c2d6c7a8bc805678bc8c30ea1e73e10c216
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-23 17:23:01 +0530

    Footer Created  And Added To The Main Layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../common/header/Navbar";
-;
+import Footer from "../common/footer/Footer";
+
 
 const MainLayout: React.FC = () => {
     return (
         <div data-theme="light">
             <Navbar />
+            <Footer />
            </div>
     );
 };
