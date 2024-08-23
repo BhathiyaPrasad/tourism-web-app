d682e5377669ebb2a704e6040147309e33d6e173
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-23 17:33:11 +0530

    added slider to main layout)(testing)

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../common/header/Navbar";
 import Footer from "../common/footer/Footer";
+import Slider from "../common/slider/Slider";
 
 
 const MainLayout: React.FC = () => {
     return (
         <div data-theme="light">
             <Navbar />
+            <Slider />
             <Footer />
            </div>
     );
