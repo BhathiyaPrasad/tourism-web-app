fd2530c709946fd621b52e04b9596629ae330aad
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-26 16:38:15 +0530

    added landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import MainLayout from "@/components/layout/MainLayout";
 import Image from "next/image";
 import dynamic from 'next/dynamic';
+import HomePackages from "@/components/product/HomePackages";
+import Footer from "../components/common/footer/Footer";
+import HomePackage from "@/components/product/HomePackage";
 
 export default function Home() {
   return (
     <div>
       <MainLayout />
-
+      <HomePackage />
+      <HomePackages />
+      <Footer />
     </div>
   );
 }
