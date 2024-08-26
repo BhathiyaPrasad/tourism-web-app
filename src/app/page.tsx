06eb2f00b75cb4966e30b9df89657a4e548c1b9a
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-26 17:07:00 +0530

    import updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import MainLayout from "@/components/layout/MainLayout";
 import Image from "next/image";
 import dynamic from 'next/dynamic';
-import HomePackages from "@/components/product/Gallery";
 import Footer from "../components/common/footer/Footer";
 import HomePackage from "@/components/product/HomePackage";
+import Gallery from "@/components/product/Gallery";
 
 export default function Home() {
   return (
     <div>
       <MainLayout />
       <HomePackage />
-      <HomePackages />
+      <Gallery />
       <Footer />
     </div>
   );
