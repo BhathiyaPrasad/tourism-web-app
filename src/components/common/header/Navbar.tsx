b27ed66a7cda57fa551c32508ab70639b001189a
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-26 13:15:10 +0530

    Extract the css style from the navbar page

--- src/components/common/header/Navbar.tsx
+++ src/components/common/header/Navbar.tsx
@@ -1,19 +1,21 @@
 'use client'
-import React from "react"
+import React from "react";
 import Link from "next/link";
-import Translate from '../../translate/Translate'
+import Translate from '../../translate/Translate';
+import Image from "next/image";
+import Logo from '../../../../public/assets/jagathlogo3.png';
+import styles from './Navbar.module.css';  // Import the CSS module
 
 const Navbar = () => {
-
     return (
         <>
-          <div className="navbar bg-transparent fixed top-0 left-0 w-full z-10">
+            <div className={`navbar fixed top-0 left-0 w-full z-10 ${styles.navbarCustom}`}>
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className={`btn btn-ghost lg:hidden ${styles.dropdownButton}`}>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
+                                className="h-6 w-6"
                                 fill="none"
                                 viewBox="0 0 24 24"
                                 stroke="currentColor">
@@ -27,34 +29,33 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><Link href='/'>Home</Link></li>
+                            <li className={styles.textWhite}><Link href='/'>Home</Link></li>
                             <li><Link href='/packages'>Packages</Link></li>
                             <li><Link href='/places'>Places</Link></li>
                             <li><Link href='/about'>About Us</Link></li>
                             <li><Link href='/contact'>Contact</Link></li>
-                            <li><Link href='/about'>About Us</Link></li>
                         </ul>
                     </div>
-                    <Link href='' className="btn btn-ghost text-xl">Jagath Travels</Link>
+                    <Link href='' className={`${styles.logoLink}`}>
+                        <Image src={Logo} alt='Jagath Travels Logo' width={120} height={62} />
+                        <span className="ml-2"></span>
+                    </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li className="text-base mr-5"><Link href='/'>Home</Link></li>
-                        <li className="text-base mr-5"><Link href='/packages'>Packages</Link></li>
-                        <li className="text-base mr-5"><Link href='/places'>Places</Link></li>
-                        <li className="text-base mr-5"><Link href='/about'>About Us</Link></li>
-                        <li className="text-base mr-5"><Link href='/contact'>Contact</Link></li>
+                        <li className={styles.menuItem}><Link href='/'>Home</Link></li>
+                        <li className={styles.menuItem}><Link href='/packages'>Packages</Link></li>
+                        <li className={styles.menuItem}><Link href='/places'>Places</Link></li>
+                        <li className={styles.menuItem}><Link href='/about'>About Us</Link></li>
+                        <li className={styles.menuItem}><Link href='/contact'>Contact</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                <Translate />
-        
-
-
+                    <Translate />
                 </div>
             </div>
         </>
     );
 };
 
-export default Navbar
+export default Navbar;
