2d55d19a6266c9049a34ae1ea3451980ca4cd236
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-28 14:29:50 +0530

    Import HomeCategoryButtons Component To Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import dynamic from 'next/dynamic';
 import Footer from "../components/common/footer/Footer";
 import HomePackage from "@/components/product/HomePackage";
 import Gallery from "@/components/product/Gallery";
+import HomeCategoryButtons from "@/components/product/CategoryButtons/HomeCategoryButtons";
 
 export default function Home() {
   return (
     <div>
       <MainLayout />
+      <HomeCategoryButtons />
       <HomePackage />
       <Gallery />
       <Footer />
