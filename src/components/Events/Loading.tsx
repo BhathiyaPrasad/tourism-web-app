034ae9d79d64683bb5df91729e42e000ab588c75
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-08-31 13:14:26 +0530

    implement Events loading

--- src/components/Events/Loading.tsx
+++ src/components/Events/Loading.tsx
@@ -7,22 +7,25 @@ const LoadingEvent = () => {
 
 
     return (
-        <div>
+        
             <section className="text-gray-600 body-font bg-white">
-                <div className="container px-5 py-22 mx-auto z-10">
-                    <div className="flex flex-wrap -m-4">
-                        <div className="p-4 w-full sm:w-1/2 md:w-1/3">
-                            <div className="flex w-full flex-col gap-4">
-                                <div className="skeleton h-32 w-full"></div>
+                <div className="container px-5 py-22 mx-auto z-10 ">
+                    <div className="flex flex-wrap  justify-center">
+                    <div className="p-4 w-full">
+                      
+                            <div className="flex flex-col gap-4 w-full">
+                                <div className="skeleton h-32 w-100"></div>
+                                <div className="skeleton h-6 w-50"></div>
                                 <div className="skeleton h-4 w-10"></div>
                                 <div className="skeleton h-4 w-10"></div>
                                 <div className="skeleton h-4 w-10"></div>
+                           
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-        </div>
+       
     )
 
 }
