85461f8625cc6e58ef92b3e7a0817ce591799018
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-03 17:31:27 +0530

    test

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainLayout from './../../components/layout/MainLayout';
 import TourTabs from '@/components/product/PackageDetails/PackageDetailsMain';
+import Navbar from '@/components/common/header/Navbar';
 
 function packages() {
   return (
     <div>
+      <Navbar />
      <MainLayout />
      <TourTabs />
     </div>
