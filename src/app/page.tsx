92c718b20303fe87f5dc414987974577d0804f78
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-03 17:10:38 +0530

    organized the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MainLayout from "@/components/layout/MainLayout";
 import Image from "next/image";
 import dynamic from 'next/dynamic';
 import Footer from "../components/common/footer/Footer";
-import HomePackage from "@/components/product/HomePackage";
+import HomePackage from "@/components/HomePackage/HomePackage";
 import Gallery from "@/components/product/gallery/Gallery";
 import HomeCategoryButtons from "@/components/product/CategoryButtons/HomeCategoryButtons";
 import EmblaCarousel from "@/components/Events/EmblaCarousel";
