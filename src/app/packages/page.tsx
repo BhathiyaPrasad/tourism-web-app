07c615b1d26bdb89f1760ee2f1793bf869e80ac5
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-09 12:49:34 +0530

    update package cards

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -2,13 +2,66 @@ import React from 'react'
 import MainLayout from './../../components/layout/MainLayout';
 import TourTabs from '@/components/product/PackageDetails/PackageDetailsMain';
 import Navbar from '@/components/common/header/Navbar';
-
+import TourPackageList from '@/components/product/PackageDetails/testpackage/TourPackageList';
 function packages() {
+  const packages = [
+  {
+    image: '',
+    name: 'Package 1',
+    description: 'This is a brief description of Package 1',
+    inclusions: ['Hotel stay', 'Meals', 'Transportation'],
+    price: 1000
+  },
+  {
+    image: '',
+    name: 'Package 1',
+    description: 'This is a brief description of Package 1',
+    inclusions: ['Hotel stay', 'Meals', 'Transportation'],
+    price: 1000
+  },
+  {
+    image: '',
+    name: 'Package 1',
+    description: 'This is a brief description of Package 1',
+    inclusions: ['Hotel stay', 'Meals', 'Transportation'],
+    price: 1000
+  },
+  {
+    image: '',
+    name: 'Package 1',
+    description: 'This is a brief description of Package 1',
+    inclusions: ['Hotel stay', 'Meals', 'Transportation'],
+    price: 1000
+  },{
+    image: '',
+    name: 'Package 1',
+    description: 'This is a brief description of Package 1',
+    inclusions: ['Hotel stay', 'Meals', 'Transportation'],
+    price: 1000
+  },{
+    image: '',
+    name: 'Package 1',
+    description: 'This is a brief description of Package 1',
+    inclusions: ['Hotel stay', 'Meals', 'Transportation'],
+    price: 1000
+  },
+  {
+    image: '',
+    name: 'Package 2',
+    description: 'This is a brief description of Package 2',
+    inclusions: ['Hotel stay', 'Meals'],
+    price: 800
+  },
+  // ...
+];
   return (
     <div>
       <Navbar />
      <MainLayout />
-    
+     <div>
+      <h1>Tour Packages</h1>
+      <TourPackageList packages={packages} />
+    </div>
     </div>
   )
 }
