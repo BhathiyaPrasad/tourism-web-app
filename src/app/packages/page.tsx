4da40f79d6be1c1ab5c3415900b16eff3e39dd16
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-09 13:21:22 +0530

    resolve conflics

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MainLayout from './../../components/layout/MainLayout';
-import TourTabs from '@/components/product/PackageDetails/PackageDetailsMain';
+
 import Navbar from '@/components/common/header/Navbar';
 import TourPackageList from '@/components/product/PackageDetails/testpackage/TourPackageList';
 function packages() {
