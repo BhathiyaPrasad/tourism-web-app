a973f4d24b7fa4dfff2d4295e423afb466a5e846
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-14 13:24:53 +0530

    added chatbox for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from './providers'
 import FloatingButton from "@/components/common/FloatingButton/FloatButton";
+import TawkToChat from "@/components/common/chat/chat"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><Providers>{children} <FloatingButton></FloatingButton></Providers></body>
+      <body className={inter.className}><Providers>{children} <FloatingButton></FloatingButton><TawkToChat /></Providers></body>
     </html>
   );
 }
