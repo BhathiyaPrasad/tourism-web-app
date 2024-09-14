c73fad722e2e2a69315f36747636458351577a81
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-14 10:48:34 +0530

    import flaoting button for layout because needs use in every instances

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from './providers'
+import FloatingButton from "@/components/common/FloatingButton/FloatButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><Providers>{children}</Providers></body>
+      <body className={inter.className}><Providers>{children} <FloatingButton></FloatingButton></Providers></body>
     </html>
   );
 }
