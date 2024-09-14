6790b81a7b5e5e2c4af57fde1d47703f6fe3c557
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-14 11:09:50 +0530

    added new button

--- src/components/common/FloatingButton/FloatButton.tsx
+++ src/components/common/FloatingButton/FloatButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { BookOpen } from 'lucide-react';
+import { CalendarCheck } from 'lucide-react';
 import {useRouter} from 'next/navigation';
 
 const FloatingButton = () => {
@@ -29,13 +29,13 @@ const FloatingButton = () => {
   return (
     <>
       {isVisible && (
-        <button
-          onClick={handleClick}
-          className="fixed bottom-4 right-4 bg-blue-700 hover:bg-blue-600 text-white font-bold py-4 px-4 rounded-full shadow-lg transition-all duration-300 flex items-center space-x-2"
-        >
-          <BookOpen size={24} />
-          <span className="hidden sm:inline">Book a Tour</span>
-        </button>
+         <button
+         onClick={handleClick}
+         className="fixed bottom-6 right-6 bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-700 hover:to-blue-900 text-white font-bold py-4 px-6 rounded-full shadow-lg transition-all duration-300 transform hover:scale-105 flex items-center space-x-3 group"
+       >
+         <CalendarCheck size={28} className="group-hover:animate-pulse" />
+         <span className="hidden sm:inline text-lg">Book a Tour</span>
+       </button>
       )}
     </>
   );
