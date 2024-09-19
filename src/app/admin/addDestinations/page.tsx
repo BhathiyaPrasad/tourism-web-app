42a83b9d7bc20ee47543bf307a39a949a1e42e1a
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-19 14:24:02 +0530

    admin destination panel added

---
+++ src/app/admin/addDestinations/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddDestination from '@/components/admin/addDestinations/AddDestination'
+
+function addDestinations(){
+
+    return (
+        <div>
+            <AddDestination /> 
+        
+        </div>
+         )
+}
+
+export default addDestinations
