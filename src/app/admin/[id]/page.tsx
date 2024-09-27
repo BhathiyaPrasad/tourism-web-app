5e3588c2e490ec111e273f4be349f04ec65ca771
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-27 16:09:45 +0530

    import the authOptionsFile

--- src/app/admin/[id]/page.tsx
+++ src/app/admin/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/authOptionsFile';
 import { redirect } from 'next/navigation';
 
 
