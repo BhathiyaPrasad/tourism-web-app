c44373794ca315adf96091e5e669ac5308104dc4
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-09-27 13:46:48 +0530

    protect the route of landing admin  page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
-
 import AdminOrderPage from '../../components/admin/orders' 
 
-function admin() {
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+
+
+
+
+
+export default async function OrdersPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    // If no session, redirect to login
+    redirect('/auth/login');
+  }
+
   return (
     <div>
-    <AdminOrderPage />
+      <h1>Admin Orders Page</h1>
+      <AdminOrderPage />
     </div>
-  )
+  );
 }
-
-export default admin
